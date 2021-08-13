371b4dbf8f0875246516d8ec36f1203433437a10
Dyeon Park <yeon0729@sookmyung.ac.kr>
2021-08-13 20:42:12 +0900

    폴더 분류 및 레이아웃 수정

--- upload/models.py
+++ upload/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+#교수자가 업로드하는 모델
 class Document(models.Model):
 	title = models.CharField(max_length = 200)
-	videoFile = models.FileField(upload_to = "Uploaded Files/")
-	docFile = models.FileField(upload_to = "Uploaded Files/")
+	videoFile = models.FileField(upload_to = "Uploaded_Files/")
+	docFile = models.FileField(upload_to = "Uploaded_Files/")
 	dateTiemOfUpload = models.DateTimeField(auto_now = True)
--- upload/views.py
+++ upload/views.py
@@ -1,23 +1,61 @@
+import os
+import shutil
+import datetime
 from . import models
+from upload.models import Document
 from django.shortcuts import render
 
 def uploadFile(request):
     if request.method == "POST":
+
         # Fetching the form data
         fileTitle = request.POST["fileTitle"]
         videoFile = request.FILES["videoFile"]
         docFile = request.FILES["docFile"]
 
+        new_path = getTime()
+
         # Saving the information in the database
         document = models.Document(
             title = fileTitle,
             videoFile = videoFile,
-            docFile = docFile
+            docFile = docFile,
+            dateTiemOfUpload = new_path
         )
         document.save()
+
+        # 사용자가 업로드한 파일 목록 불러오기
+        original_path = "D:/commentor/media/Uploaded_Files/"
+        files = os.listdir(original_path)
+        
+        # 디렉토리 생성
+        new_path = "D:/commentor/media/" + new_path + "/"        
+        createFolder(new_path)
+
+        # 파일 이동
+        for file in files:
+            shutil.move(original_path + file, new_path + file)
+
+        # 모델 내 path(url) 수정
+        obj = Document.objects.get(title = fileTitle)
+        obj.videoFile = new_path + "lecture_video.mp4"
+        obj.docFile = new_path + "lecture_doc.pdf"
+        obj.save()
 
     documents = models.Document.objects.all()
 
     return render(request, "upload/upload_file.html", context = {
         "files": documents
     })
+
+def createFolder(dir):
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    except OSError:
+        print ('Error: Creating directory. ' +  dir)
+
+def getTime():
+    now = datetime.datetime.now()
+    nowDatetime = now.strftime('%Y-%m-%d %H-%M-%S')
+    return nowDatetime
