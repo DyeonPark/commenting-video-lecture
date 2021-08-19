a3d6a4316141725293024bf2619d3e36daf29ba7
Dyeon Park <yeon0729@sookmyung.ac.kr>
2021-08-19 21:23:09 +0900

    0819 학생화면 레이아웃 구성 완료

--- onlineclass/views.py
+++ onlineclass/views.py
@@ -1,7 +1,25 @@
 from upload.models import Document
 from onlineclass.models import Helper
+
 from django.http import HttpResponse
 from django.template import loader
+
+from django.http import FileResponse
+from django.core.files.storage import FileSystemStorage
+
+
+def download_pdf(request, doc_id):
+    doc = Document.objects.get(id=doc_id)
+
+    file_path = doc.docFile.path
+    fs = FileSystemStorage(file_path)
+
+    set_filename = doc.title + " 강의자료.pdf"  # 파일명 지정
+    print(set_filename)
+
+    response = FileResponse(fs.open(file_path, 'rb'))
+    response['Content-Disposition'] = 'attachment; filename=' + set_filename  # 파일명 지정이 안되는 오류 발생
+    return response
 
 
 def execute_commentor(request, doc_id):
